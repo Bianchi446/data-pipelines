7eb0d590e44e60cf6c13899c1cdf6e7d2008b522
Bianchi446 <josebianchi221js@gmail.com>
2023-12-18 13:59:41 -0300

    Fetching pages

---
+++ ch4/dags/listing_4_8.py
@@ -0,0 +1,22 @@
+import airflow.utils.dates
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+dag = DAG(
+    dag_id = "listing_4_08",
+    start_date = airflow.utils.dates.days_ago(3),
+    schedule = "@daily",
+    )
+
+def _print_context(**context):
+    start = context["execution_date"]
+    end = context["next_execution_date"]
+    print(f"Start : {start}, end: {end}")
+
+print_context = PythonOperator(
+    task_id = "print_context",
+    python_callable = _print_context,
+    dag=dag
+)
+
+~                                            
